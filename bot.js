d9598078787db864016759505ced9b256334e3d3
Jaime Revello <jrexello@gmail.com>
2017-11-13 10:36:35 +0100

    added play

--- bot.js
+++ bot.js
@@ -42,6 +42,29 @@ client.on("message", message => {
       message.channel.send("Me gustan grandes");
     }
 
+    else if(mensaje === "play"){
+      let videoUrl = message.content.split(" ");
+			const voiceChannel = message.member.voiceChannel;
+			if (videoUrl[1] == undefined) {
+				return message.reply("pon un enlace al menos.");
+			}
+			if (!voiceChannel) {
+				return message.reply("debes estar en un canal de voz");
+			} else {
+				message.reply("Reproduciendo " + videoUrl[1]);
+				message.delete();
+			}
+			voiceChannel.join()
+				.then(connnection => {
+					const stream = ytdl(videoUrl[1], {
+						filter: 'audioonly'
+					});;
+					const dispatcher = connnection.playStream(stream);
+					dispatcher.on('end', () => voiceChannel.leave());
+				});
+
+    }
+
     else if(mensaje.match(/creaRaid \d/i)){
       var numero = Number(mensaje.substring(mensaje.length-2));
       var arrRaid = [message.author];
